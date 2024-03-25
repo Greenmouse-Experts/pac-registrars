3ceeff4a110f4a6f03ca8aa32af90d1c713ef036
iammoses <inawo.moses@gmail.com>
2024-03-25 13:25:27 +0100

    starter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <p className='text-2xl text-red-400'>name</p>
+    </>
+  )
+}
+
+export default App
