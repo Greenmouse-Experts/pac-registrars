83a57f026ae44df626b3a29dfb292850b7c2b254
iammoses <inawo.moses@gmail.com>
2024-05-21 14:13:15 +0100

    header adjusted

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
 
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 interface BannerProps {
@@ -8,17 +9,17 @@ interface BannerProps {
 
 function Banner({ page }: BannerProps) {
   return (
-    <section className="aboutTop  bg-cover bg-center flex justify-center items-start flex-col relative">
+    <section className="bg-[url('/images/about-banner.png')] h-[22rem]  bg-cover bg-center flex justify-center items-start flex-col relative">
       <h4 className="2xl:text-6xl text-3xl font-bold font-montserrat 2xl:leading-[88.2px] leading-[70px] capitalize text-white mt-5 2xl:mt-0">
         {page}
       </h4>
       <div>
-        <p className="text-white font-syne">
+        <p className="text-white font-syne flex items-center">
           <NavLink className="font-syne" to="/">
             Home
           </NavLink>
-          &nbsp;
-          {">"}&nbsp;<span className="font-syne text-[14px]"> {page} </span>
+         <span>
+          <MdOutlineKeyboardArrowRight color="white" /></span><span className="font-syne text-[14px]"> {page} </span>
         </p>
       </div>
       
