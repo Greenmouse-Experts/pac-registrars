85ea05868b3be684ee54580d15e74c6543ff28a3
iammoses <inawo.moses@gmail.com>
2024-05-22 11:30:02 +0100

    adjusted

--- src/components/HomeComp/Services.tsx
+++ src/components/HomeComp/Services.tsx
@@ -17,7 +17,7 @@ function Services() {
     autoplaySpeed: 2000,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1508,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -26,7 +26,7 @@ function Services() {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -34,7 +34,7 @@ function Services() {
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 700,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
