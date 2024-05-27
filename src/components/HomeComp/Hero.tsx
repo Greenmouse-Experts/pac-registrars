5f9327107c3bb61039521d90afb6f336d880fd58
iammoses <inawo.moses@gmail.com>
2024-05-27 11:35:36 +0100

    adjusted

--- src/components/HomeComp/Hero.tsx
+++ src/components/HomeComp/Hero.tsx
@@ -1,15 +1,5 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-// import { NavLink } from "react-router-dom";
-// import hero1 from "../assets/hero-1.png";
-// import hero2 from "../assets/hero-2.png";
-// import hero3 from "../assets/hero-3.png";
-// import hero4 from "../assets/hero-4.png";
-// const hero1 = "https://asqyfzgqnbxthtasplew.supabase.co/storage/v1/object/public/cabin-images/hero-1.png"
-// const hero2 = "https://asqyfzgqnbxthtasplew.supabase.co/storage/v1/object/public/cabin-images/hero-2.png"
-// const hero3 = "https://asqyfzgqnbxthtasplew.supabase.co/storage/v1/object/public/cabin-images/hero-3.png"
-// const hero4 = "https://asqyfzgqnbxthtasplew.supabase.co/storage/v1/object/public/cabin-images/hero-4.png"
 
 function Hero() {
   const [currentSlide, setCurrentSlide] = useState(1);
@@ -49,7 +39,7 @@ function Hero() {
             {/* <p className="2xl:text-[17px] 2xl:font-[400] font-semibold text-white font-syne">
           Take control of your financial future and start growing your wealth
         </p> */}
-            <h4 className="2xl:text-[55px] lg:text-[45px] text-[30px]  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
+            <h4 className="2xl:text-[3.5rem] lg:text-[3rem] text-3xl  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
               Streamline Operations with Our Company Secretarial Services
             </h4>
             <button className="bg-white text-bluePrimary mt-10 px-10 py-3 font-semibold hover:bg-bluePrimary hover:text-white transition-all">
@@ -66,7 +56,7 @@ function Hero() {
             {/* <p className="2xl:text-[17px] 2xl:font-[400] font-semibold text-white font-syne">
           Take control of your financial future and start growing your wealth
         </p> */}
-            <h4 className="2xl:text-[55px] lg:text-[45px] text-[30px]  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
+            <h4 className="2xl:text-[3.5rem] lg:text-[3rem] text-3xl  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
               Enhance Your Business with Our Registrar Services
             </h4>
             <button className="bg-white text-bluePrimary mt-10 px-10 py-3 font-semibold hover:bg-bluePrimary hover:text-white transition-all">
@@ -83,7 +73,7 @@ function Hero() {
             {/* <p className="2xl:text-[17px] 2xl:font-[400] font-semibold text-white font-syne">
           Take control of your financial future and start growing your wealth
         </p> */}
-            <h4 className="2xl:text-[55px] lg:text-[45px] text-[30px]  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
+            <h4 className="2xl:text-[3.5rem] lg:text-[3rem] text-3xl  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
               Navigate Probate with Our Expert Guidance
             </h4>
             <button className="bg-white text-bluePrimary mt-10 px-10 py-3 font-semibold hover:bg-bluePrimary hover:text-white transition-all">
@@ -101,7 +91,7 @@ function Hero() {
             {/* <p className="2xl:text-[17px] 2xl:font-[400] font-semibold text-white font-syne">
           Take control of your financial future and start growing your wealth
         </p> */}
-            <h4 className="2xl:text-[55px] lg:text-[45px] text-[30px]  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
+            <h4 className="2xl:text-[3.5rem] lg:text-[3rem] text-3xl  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
               Optimize Efficiency with Our Document Management Solutions
             </h4>
             <button className="bg-white text-bluePrimary mt-10 px-10 py-3 font-semibold hover:bg-bluePrimary hover:text-white transition-all">
@@ -118,7 +108,7 @@ function Hero() {
             {/* <p className="2xl:text-[17px] 2xl:font-[400] font-semibold text-white font-syne">
           Take control of your financial future and start growing your wealth
         </p> */}
-            <h4 className="2xl:text-[55px] lg:text-[45px] text-[30px]  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
+            <h4 className="2xl:text-[3.5rem] lg:text-[3rem] text-3xl  font-bold font-montserrat 2xl:leading-[70.2px] lg:leading-[60px] leading-[40px] capitalize text-white mt-5 sm:mt-0 ">
               Achieve Financial Growth with Our Innovative Solutions
             </h4>
             <button className="bg-white text-bluePrimary mt-10 px-10 py-3 font-semibold hover:bg-bluePrimary hover:text-white transition-all">
