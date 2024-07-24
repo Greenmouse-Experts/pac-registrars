b2f2bd8442cec0a879ce8b3c055b6301ccaf2e0a
iammoses <inawo.moses@gmail.com>
2024-07-24 15:06:41 +0100

    updated

--- src/pages/Management/index.tsx
+++ src/pages/Management/index.tsx
@@ -75,7 +75,8 @@ const ManagementBio: IBio[] = [
     name: "Boluwatife Efeoghene",
     title: "...",
     bio: [
-      "...",
+      "Boluwatife Efeoghene is an investment banker with 1 decade of experience within the capital market. Her experience cuts across Customer service operations, finance, relationship management, and business development.",
+      "She holds B.Sc. from the National Open University of Nigeria and an MBA from Ladoke Akintola University of Technology. She is an Associate member of the Institute of Capital Market Registrars (ICMR). She is currently the Team Lead, Operations prior to her joining PAC Registrars and Investor Services, she worked with Africa Prudential Plc."
     ],
     image: "/images/leader-11.png",
   },
